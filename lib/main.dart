bfac1609ca57b0200655654b8872f4106c1d5aae
G1Joshi <joshijeevan98@gmail.com>
2021-06-17 15:33:15 +0530

    update UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:pop_shop/home.dart';
+import 'package:pop_shop/views/home.dart';
 
 void main() {
   runApp(PopShop());
@@ -12,7 +12,7 @@ class PopShop extends StatelessWidget {
       title: 'Pop Shop',
       theme: ThemeData(
         primaryColor: Color(0xFF1e90ff),
-        accentColor: Color(0xFF009fff),
+        accentColor: Color(0xFF00bfff),
       ),
       home: Home(),
     );
